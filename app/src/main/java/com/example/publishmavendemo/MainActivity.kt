227aaaabacdfdc6c1f7a7d797057bdcdd4078e26
guofeng <guofeng@yeelight.com>
2021-07-15 14:43:29 +0800

    Add description

--- app/src/main/java/com/example/publishmavendemo/MainActivity.kt
+++ app/src/main/java/com/example/publishmavendemo/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
 import com.example.publishmavendemo.databinding.ActivityMainBinding
+import com.guof.dog.DogFactory
 
 class MainActivity : AppCompatActivity() {
 
--- doglibrary/src/main/java/com/guof/dog/Dog.kt
+++ doglibrary/src/main/java/com/guof/dog/Dog.kt
@@ -2,6 +2,9 @@ package com.guof.dog
 
 class Dog constructor(var name: String, var age: String) {
 
+    /**
+     * it's a description
+     */
     fun eat(){
         println("$name eat something")
     }
